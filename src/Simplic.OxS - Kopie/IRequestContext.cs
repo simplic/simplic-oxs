c6a9f2b06793064e5312b3cbd03f569c344e11ad
Benedikt Eggers <be@spiegelburg.net>
2022-05-30 15:59:44 +0200

    Remove wrong copy

--- src/Simplic.OxS - Kopie/IRequestContext.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Simplic.OxS
-{
-    public interface IRequestContext
-    {
-        public Guid? UserId { get; }
-        public Guid? TenantId { get; }
-        public Guid? CorrelationId { get; }
-    }
-}

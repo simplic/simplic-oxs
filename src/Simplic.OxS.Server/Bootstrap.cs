b9b9825d1b5922b283d74b5ccfc6e3b00765b7ee
Benedikt Eggers <be@spiegelburg.net>
2022-06-16 11:53:08 +0200

    Add SignalR

--- src/Simplic.OxS.Server/Bootstrap.cs
+++ src/Simplic.OxS.Server/Bootstrap.cs
@@ -112,6 +112,7 @@ namespace Simplic.OxS.Server
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint($"{basePath}/swagger/{ApiVersion}/swagger.json", $"Simplic.OxS.{ServiceName} {ApiVersion}");
+                c.SwaggerEndpoint($"{basePath}/swagger/{ApiVersion}-SignalR/swagger.json", $"Simplic.OxS.{ServiceName} {ApiVersion}");
             });
 
             app.UseHttpsRedirection();

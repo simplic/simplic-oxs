c20e42974d40e65e52c4aaecd2592459b8bf14b3
Benedikt Eggers <be@spiegelburg.net>
2022-07-04 22:34:08 +0200

    Format

--- src/Simplic.OxS.Data/MapService/MapService.cs
+++ src/Simplic.OxS.Data/MapService/MapService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Simplic.OxS.Data
+﻿namespace Simplic.OxS.Data
 {
     /// <inheritdoc>
     public class MapService : IMapService

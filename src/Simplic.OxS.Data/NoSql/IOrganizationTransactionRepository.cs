91601afa7012f0702f3e114e6dcce4525b11614b
juliusfunke <contact@juliusfunke.com>
2022-07-18 18:31:21 +0200

    Add OrganizationTransactionServiceBase, IOrganizationTransactionRepository

---
+++ src/Simplic.OxS.Data/NoSql/IOrganizationTransactionRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Simplic.OxS.Data
+{
+    /// <summary>
+    /// Basic repository
+    /// </summary>
+    /// <typeparam name="TId">PK (ID) type</typeparam>
+    /// <typeparam name="TDocument">Entity type</typeparam>
+    /// <typeparam name="TFilter">Filter type</typeparam>
+    public interface IOrganizationTransactionRepository<TId, TDocument, TFilter> : IRepository<TId, TDocument, TFilter>, ITransactionRepository<TDocument, TFilter>
+        where TDocument : IOrganizationDocument<TId>, new ()
+        where TFilter : IOrganizationFilter<TId>
+    {
+        /// <summary>
+        /// Get an entity by its id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="queryAllOrganizations"></param>
+        /// <returns>Entity</returns>
+        Task<TDocument> GetAsync(TId id, bool queryAllOrganizations = false);
+    }
+}

5b5434848da31ce65ba8584618440957825cfdc0
juliusfunke <contact@juliusfunke.com>
2022-07-22 15:05:33 +0200

    Improve inheritance structure

--- src/Simplic.OxS.Data/NoSql/IOrganizationTransactionRepository.cs
+++ src/Simplic.OxS.Data/NoSql/IOrganizationTransactionRepository.cs
@@ -6,16 +6,10 @@
     /// <typeparam name="TId">PK (ID) type</typeparam>
     /// <typeparam name="TDocument">Entity type</typeparam>
     /// <typeparam name="TFilter">Filter type</typeparam>
-    public interface IOrganizationTransactionRepository<TId, TDocument, TFilter> : IRepository<TId, TDocument, TFilter>, ITransactionRepository<TDocument, TId>
+    public interface IOrganizationTransactionRepository<TId, TDocument, TFilter> : IOrganizationRepository<TId, TDocument, TFilter>, ITransactionRepository<TDocument, TId>
         where TDocument : IOrganizationDocument<TId>, new ()
         where TFilter : IOrganizationFilter<TId>
     {
-        /// <summary>
-        /// Get an entity by its id
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="queryAllOrganizations"></param>
-        /// <returns>Entity</returns>
-        Task<TDocument> GetAsync(TId id, bool queryAllOrganizations = false);
+
     }
 }
--- src/Simplic.OxS.Data/Service/OrganizationTransactionServiceBase.cs
+++ src/Simplic.OxS.Data/Service/OrganizationTransactionServiceBase.cs
@@ -2,41 +2,17 @@
 
 namespace Simplic.OxS.Data.Service
 {
-    public class OrganizationTransactionServiceBase<TDocument, TFilter> : IOrganizationTransactionServiceBase<TDocument, TFilter>
+    public class OrganizationTransactionServiceBase<TDocument, TFilter> : OrganizationServiceBase<TDocument, TFilter>, IOrganizationTransactionServiceBase<TDocument, TFilter>
         where TDocument : IOrganizationDocument<Guid>, new ()
         where TFilter : IOrganizationFilter<Guid>
     {
         private readonly IOrganizationTransactionRepository<Guid, TDocument, TFilter> repository;
         private readonly IRequestContext requestContext;
 
-        public OrganizationTransactionServiceBase(IOrganizationTransactionRepository<Guid, TDocument, TFilter> repository, IRequestContext requestContext)
+        public OrganizationTransactionServiceBase(IOrganizationTransactionRepository<Guid, TDocument, TFilter> repository, IRequestContext requestContext) : base(repository, requestContext)
         {
             this.repository = repository;
             this.requestContext = requestContext;
-        }
-
-        public virtual async Task<TDocument> GetById(Guid id) => await repository.GetAsync(id);
-
-        public virtual async Task<TDocument> Create([NotNull] TDocument obj)
-        {
-            AssertRequest(obj, false);
-
-            obj.Id = Guid.NewGuid();
-            obj.OrganizationId = requestContext.OrganizationId.Value;
-
-            if (obj is IDocumentDataExtension ext)
-            {
-                ext.CreateDateTime = DateTime.UtcNow;
-                ext.CreateUserId = requestContext.UserId;
-
-                ext.UpdateDateTime = ext.CreateDateTime;
-                ext.UpdateUserId = ext.CreateUserId;
-            }
-
-            await repository.CreateAsync(obj);
-            await repository.CommitAsync();
-
-            return obj;
         }
 
         public virtual async Task<TDocument> Create([NotNull] TDocument obj, ITransaction transaction)
@@ -60,25 +36,6 @@ namespace Simplic.OxS.Data.Service
             return obj;
         }
 
-        public virtual async Task<TDocument> Update([NotNull] TDocument obj)
-        {
-            AssertRequest(obj, true);
-
-            if (obj.Id == default)
-                throw new Exception("Id must be set for updating data.");
-
-            if (obj is IDocumentDataExtension ext)
-            {
-                ext.UpdateDateTime = DateTime.UtcNow;
-                ext.UpdateUserId = requestContext.UserId;
-            }
-
-            await repository.UpdateAsync(obj);
-            await repository.CommitAsync();
-
-            return obj;
-        }
-
         public virtual async Task<TDocument> Update([NotNull] TDocument obj, ITransaction transaction)
         {
             AssertRequest(obj, true);
@@ -93,24 +50,6 @@ namespace Simplic.OxS.Data.Service
             }
 
             await repository.UpdateAsync(obj, transaction);
-
-            return obj;
-        }
-
-        public virtual async Task<TDocument> Delete([NotNull] TDocument obj)
-        {
-            AssertRequest(obj, true);
-
-            obj.IsDeleted = true;
-
-            if (obj is IDocumentDataExtension ext)
-            {
-                ext.UpdateDateTime = DateTime.UtcNow;
-                ext.UpdateUserId = requestContext.UserId;
-            }
-
-            await repository.UpdateAsync(obj);
-            await repository.CommitAsync();
 
             return obj;
         }
@@ -130,13 +69,6 @@ namespace Simplic.OxS.Data.Service
             await repository.UpdateAsync(obj, transaction);
 
             return obj;
-        }
-
-        public virtual async Task Delete(Guid id)
-        {
-            var obj = await GetById(id);
-            if (obj != null)
-                await Delete(obj);
         }
 
         public virtual async Task Delete(Guid id, ITransaction transaction)

49330bed0dd38c717143c2c98c17d793256fe3aa
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2022-10-11 09:53:36 +0200

    add unit tests for simple test cases

---
+++ src/Simplic.OxS.Server.Test/PatchTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.Server.Test
+{
+    public class PatchHelperTest
+    {
+        [Fact]
+        public async Task Patch_SingleFieldJson_PatchesSingleField()
+        {
+            var originalTestPerson = new TestPerson
+            {
+                FirstName = "John",
+                LastName = "Mustermann"
+            };
+
+            var mappedTestPerson = new TestPerson
+            {
+                LastName = "Doe"
+            };
+
+            var json = @"{""LastName"" : ""Doe""}";
+
+            var patchedTestPerson = PatchHelper.Patch(originalTestPerson, mappedTestPerson, json, (validation) =>
+            {
+                return true;
+            });
+
+            patchedTestPerson.LastName.Should().Be("Doe");
+        }
+
+        [Fact]
+        public async Task Patch_AllFieldJson_PatchesMultipleFields()
+        {
+            var originalTestPerson = new TestPerson
+            {
+                FirstName = "Max",
+                LastName = "Mustermann"
+            };
+
+            var mappedTestPerson = new TestPerson
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var json = @"{
+                            ""FirstName"": ""John"",
+                            ""LastName"": ""Doe""
+                        }";
+
+            var patchedTestPerson = PatchHelper.Patch(originalTestPerson, mappedTestPerson, json, (validation) =>
+            {
+                return true;
+            });
+
+            patchedTestPerson.FirstName.Should().Be("John");
+            patchedTestPerson.LastName.Should().Be("Doe");
+        }
+
+        [Fact]
+        public async Task Patch_SingleFieldJsonMultiFieldMap_JustPatchesJsonFields()
+        {
+            var originalTestPerson = new TestPerson
+            {
+                FirstName = "John",
+                LastName = "Mustermann"
+            };
+
+            var mappedTestPerson = new TestPerson
+            {
+                FirstName = "Max",
+                LastName = "Doe"
+            };
+
+            var json = @"{""LastName"" : ""Doe""}";
+
+            var patchedTestPerson = PatchHelper.Patch(originalTestPerson, mappedTestPerson, json, (validation) =>
+            {
+                return true;
+            });
+
+            patchedTestPerson.LastName.Should().Be("Doe");
+            patchedTestPerson.FirstName.Should().Be("John");
+        }
+    }
+}
---
+++ src/Simplic.OxS.Server.Test/TestDataClasses/TestPerson.cs
@@ -0,0 +1,20 @@
+﻿using Simplic.OxS.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.Server.Test
+{
+    public class TestPerson : IDocument<Guid>
+    {
+        public Guid Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+    }
+}

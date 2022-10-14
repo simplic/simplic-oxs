c5c579397aa2b48aced79c552dcecd06520a9948
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2022-10-14 11:34:26 +0200

    Move patch helper to subdir and move validation requests out of temp class

---
+++ src/Simplic.OxS.Server/Helper/ValidationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.Server
+{
+    public class ValidationRequest
+    {
+        public string Property { get; set; }
+        public string Path { get; set; }
+        public object Value { get; set; }
+    }
+}

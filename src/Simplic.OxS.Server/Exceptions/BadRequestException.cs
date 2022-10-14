90c827fd345d2680ec36d7015419cbfde0b40601
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2022-10-14 07:58:22 +0200

    Add a bad request excetion to oxs base

---
+++ src/Simplic.OxS.Server/Exceptions/BadRequestException.cs
@@ -0,0 +1,19 @@
+﻿namespace Simplic.OxS.Server
+{
+    /// <summary>
+    /// Exception to throw when the request is bad, or not as it should be.
+    /// <para>
+    /// Should be catched in the controllers to return a bad request instead of an internal server error.
+    /// </para>
+    /// </summary>
+    public class BadRequestException : Exception
+    {
+        /// <summary>
+        /// Initializes a new bad request exception.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        public BadRequestException(string? message) : base(message)
+        {
+        }
+    }
+}

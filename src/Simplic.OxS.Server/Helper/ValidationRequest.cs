0dd86d18170b3de90d90881d8fa14f6e38f4d682
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2022-11-11 14:26:48 +0100

    Add validation request type

--- src/Simplic.OxS.Server/Helper/ValidationRequest.cs
+++ src/Simplic.OxS.Server/Helper/ValidationRequest.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Simplic.OxS.Server
+﻿namespace Simplic.OxS.Server
 {
+    /// <summary>
+    /// Represents a validation request send for each patched property.
+    /// </summary>
     public class ValidationRequest
     {
+        /// <summary>
+        /// Gets or sets the property name.
+        /// </summary>
         public string Property { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full path.
+        /// </summary>
         public string Path { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value that will be set, when available.
+        /// </summary>
         public object Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the validation request.
+        /// </summary>
+        public ValidationRequestType Type { get; set; }
     }
 }

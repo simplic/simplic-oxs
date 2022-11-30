94d6555e63f8c6018a5fea3925919151715c773b
juliusfunke <contact@juliusfunke.com>
2022-11-30 16:53:26 +0100

    Remove unnecessary code

--- src/Simplic.OxS.ResourceLocking/ResourceLockingExtension.cs
+++ src/Simplic.OxS.ResourceLocking/ResourceLockingExtension.cs
@@ -16,24 +16,8 @@ namespace Simplic.OxS.ResourceLocking
         /// <param name="services">Service collection</param>
         /// <param name="Assemblies">List of assemblies that should be loaded for reading hook definitions</param>
         /// <returns>Service collection instance</returns>
-        public static IServiceCollection AddResourceLocking(this IServiceCollection services, IConfiguration configuration, IList<string>? assemblies = null)
+        public static IServiceCollection AddResourceLocking(this IServiceCollection services, IConfiguration configuration)
         {
-            if (assemblies != null)
-            {
-                foreach (var assemblyName in assemblies)
-                {
-                    Console.WriteLine($"Load assembly for retreiving hook definitions: {assemblyName}");
-                    try
-                    {
-                        Assembly.Load(assemblyName);
-                    }
-                    catch (Exception ex)
-                    {
-                        throw new Exception($"Could not load assembly {assemblyName}", ex);
-                    }
-                }
-            }
-
             var redisUrl = configuration.GetValue<string>("Redis:RedisCacheUrl");
             var existingConnection = ConnectionMultiplexer.Connect(redisUrl);
 

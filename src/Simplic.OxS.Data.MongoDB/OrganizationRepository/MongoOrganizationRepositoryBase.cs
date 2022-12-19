5719158e618884ef8d37951d75d91a5f0950031c
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2022-12-19 16:05:18 +0100

    fix oid in find async

--- src/Simplic.OxS.Data.MongoDB/OrganizationRepository/MongoOrganizationRepositoryBase.cs
+++ src/Simplic.OxS.Data.MongoDB/OrganizationRepository/MongoOrganizationRepositoryBase.cs
@@ -31,6 +31,37 @@ namespace Simplic.OxS.Data.MongoDB
             return data.SingleOrDefault();
         }
 
+        /// <summary>
+        /// Finds the documents matching the filter.
+        /// </summary>
+        /// <param name="predicate">The filter predicate</param>
+        /// <param name="skip">Number of skipped entities</param>
+        /// <param name="limit">Number of requested entities</param>
+        /// <param name="sortField">Sort field</param>
+        /// <param name="isAscending">Ascending or Descending sort</param>
+        /// <returns><see cref="TDocument"/> entities matching the search criteria</returns>
+        public async override Task<IEnumerable<TDocument>> FindAsync(TFilter predicate, int? skip, int? limit, string sortField = "", bool isAscending = true, Collation collation = null)
+        {
+            await Initialize();
+
+            predicate.OrganizationId = predicate.OrganizationId.HasValue
+                 ? predicate.OrganizationId
+                 : predicate.QueryAllOrganizations
+                    ? null
+                    : requestContext.OrganizationId;
+
+            SortDefinition<TDocument> sort = null;
+            if (!string.IsNullOrWhiteSpace(sortField))
+                sort = isAscending
+                    ? Builders<TDocument>.Sort.Ascending(sortField)
+                    : Builders<TDocument>.Sort.Descending(sortField);
+            var findOptions = new FindOptions();
+            if (collation != null)
+                findOptions.Collation = collation;
+            return Collection.Find(BuildFilterQuery(predicate), findOptions).Sort(sort).Skip(skip).Limit(limit).ToList();
+
+        }
+
         public async Task<IEnumerable<TDocument>> GetAllAsync()
         {
             return await GetAllAsync(false);

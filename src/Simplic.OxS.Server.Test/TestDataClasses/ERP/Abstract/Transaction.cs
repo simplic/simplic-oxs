64780f7e463a8a75bd3cf72ac28553005eed6ee9
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2023-03-14 15:33:25 +0100

    more comments

--- src/Simplic.OxS.Server.Test/TestDataClasses/ERP/Abstract/Transaction.cs
+++ src/Simplic.OxS.Server.Test/TestDataClasses/ERP/Abstract/Transaction.cs
@@ -1,11 +1,13 @@
 ﻿namespace Simplic.OxS.Server.Test.TestDataClasses.ERP.Abstract
 {
+    /// <summary>
+    /// Just a sample class to test items in items behaviour.
+    /// </summary>
     public class Transaction
     {
         /// <summary>
         /// Gets or sets the items of this transaction.
         /// </summary>
         public IList<TransactionItem> Items { get; set; } = new List<TransactionItem>();
-
     }
 }
--- src/Simplic.OxS.Server.Test/TestDataClasses/ERP/Abstract/TransactionItemType.cs
+++ src/Simplic.OxS.Server.Test/TestDataClasses/ERP/Abstract/TransactionItemType.cs
@@ -1,9 +1,18 @@
 ﻿namespace Simplic.OxS.Server.Test.TestDataClasses.ERP.Abstract
 {
+    /// <summary>
+    /// Just a sample type to fill for easier assertion.
+    /// </summary>
     public class TransactionItemType
     {
+        /// <summary>
+        /// Id to check in assertion.
+        /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Name for easier debugging.
+        /// </summary>
         public string Name { get; set; }
     }
 }

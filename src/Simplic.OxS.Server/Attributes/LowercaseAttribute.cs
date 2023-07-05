2374127cdf87cb9e1a7ae549023b11ef4cedc665
Benedikt Eggers <be@spiegelburg.net>
2023-07-05 14:49:58 +0200

    Add lower case validation property

---
+++ src/Simplic.OxS.Server/Attributes/LowercaseAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Simplic.OxS.Server;
+
+/// <summary>
+/// When used over a property in a asp.net core model property, its content will be converted
+/// to lowercase.
+/// </summary>
+public class LowercaseAttribute : ValidationAttribute
+{
+	/// <summary>
+	/// Execute validation and turn content into lower-case
+	/// </summary>
+	/// <param name="value">Input value for validation</param>
+	/// <param name="validationContext">Current validation context.</param>
+	/// <returns>Always success as validation resul</returns>
+	protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+	{
+		if (value != null && validationContext.MemberName != null)
+		{
+			string? lowercaseValue = value.ToString()?.ToLower();
+
+			if(lowercaseValue == null) 
+				return ValidationResult.Success;
+
+			validationContext.ObjectType.GetProperty(validationContext.MemberName)?
+										.SetValue(validationContext.ObjectInstance, lowercaseValue);
+		}
+
+		return ValidationResult.Success;
+	}
+}

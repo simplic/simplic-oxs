b44f9c11d3389ba0e204c9313adf0e43e9da0356
Benedikt Eggers <be@spiegelburg.net>
2023-10-24 11:16:34 +0200

    Clean code

--- src/Simplic.OxS.Scheduler/AsyncScopedJob.cs
+++ src/Simplic.OxS.Scheduler/AsyncScopedJob.cs
@@ -3,11 +3,18 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Simplic.OxS.Scheduler
 {
+    /// <summary>
+    /// Hangfire job that execute a <see cref="IAsyncScopedJobService"/> and creates its own DI scope
+    /// </summary>
     public class AsyncScopedJob : IDisposable
     {
         private readonly IServiceScope serviceScope;
         private readonly IRequestContext requestContext;
 
+        /// <summary>
+        /// Initialize new job and create new DI scope
+        /// </summary>
+        /// <param name="serviceScopeFactory">Scope factory for creating a new DI-scope</param>
         public AsyncScopedJob([NotNull] IServiceScopeFactory serviceScopeFactory)
         {
             serviceScope = serviceScopeFactory.CreateScope();
@@ -15,6 +22,12 @@ namespace Simplic.OxS.Scheduler
             requestContext = serviceScope.ServiceProvider.GetService<IRequestContext>();
         }
 
+        /// <summary>
+        /// Execute job async
+        /// </summary>
+        /// <param name="jobService">Job to execute as type. The type must be registered using services.AddTransient before.</param>
+        /// <param name="parameter">Parameter for passing e.g. user-id, organization-id, etv.</param>
+        /// <exception cref="Exception">Throws an exception, if the jobService is not registered</exception>
         public async Task ExecuteJobAsync(Type jobService, ScopeJobParameter parameter)
         {
             // Set requestContext data for the current scope
@@ -38,13 +51,12 @@ namespace Simplic.OxS.Scheduler
             }
         }
 
+        /// <summary>
+        /// Dispose object and clear scope
+        /// </summary>
         public void Dispose()
         {
             serviceScope.Dispose();
         }
-
-        public string Name { get; set; }
-
-        public IRequestContext RequestContext { get => requestContext; }
     }
 }
--- src/Simplic.OxS.Scheduler/ScopedJob.cs
+++ src/Simplic.OxS.Scheduler/ScopedJob.cs
@@ -3,11 +3,18 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Simplic.OxS.Scheduler
 {
+    /// <summary>
+    /// Hangfire job that execute a <see cref="IScopedJobService"/> and creates its own DI scope
+    /// </summary>
     public class ScopedJob : IDisposable
     {
         private readonly IServiceScope serviceScope;
         private readonly IRequestContext requestContext;
 
+        /// <summary>
+        /// Initialize new job and create new DI scope
+        /// </summary>
+        /// <param name="serviceScopeFactory">Scope factory for creating a new DI-scope</param>
         public ScopedJob([NotNull] IServiceScopeFactory serviceScopeFactory)
         {
             serviceScope = serviceScopeFactory.CreateScope();
@@ -15,6 +22,12 @@ namespace Simplic.OxS.Scheduler
             requestContext = serviceScope.ServiceProvider.GetService<IRequestContext>();
         }
 
+        /// <summary>
+        /// Execute job sync
+        /// </summary>
+        /// <param name="jobService">Job to execute as type. The type must be registered using services.AddTransient before.</param>
+        /// <param name="parameter">Parameter for passing e.g. user-id, organization-id, etv.</param>
+        /// <exception cref="Exception">Throws an exception, if the jobService is not registered</exception>
         public void ExecuteJob(Type jobService, ScopeJobParameter parameter)
         {
             // Set requestContext data for the current scope
@@ -38,13 +51,12 @@ namespace Simplic.OxS.Scheduler
             }
         }
 
+        /// <summary>
+        /// Dispose object and clear scope
+        /// </summary>
         public void Dispose()
         {
             serviceScope.Dispose();
         }
-
-        public string Name { get; set; }
-
-        public IRequestContext RequestContext { get => requestContext; }
     }
 }

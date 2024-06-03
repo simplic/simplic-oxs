6598c34ad2bf18dc0dbd1f470050396e65f9d33e
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2024-06-03 16:06:37 +0200

    try something new

--- src/Simplic.OxS.Server/Bootstrap.cs
+++ src/Simplic.OxS.Server/Bootstrap.cs
@@ -19,7 +19,7 @@ using Simplic.OxS.ModelDefinition.Extension;
 namespace Simplic.OxS.Server
 {
     /// <summary>
-    /// Base class for implementing a Simplic.OxS microservice
+    /// Base class for implementing a Simplic.OxS microservice 
     /// </summary>
     public abstract class Bootstrap
     {
@@ -113,7 +113,7 @@ namespace Simplic.OxS.Server
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            
             app.UseSwagger(c =>
             {
                 c.PreSerializeFilters.Add((swagger, httpReq) =>

95524ae3c17cbd34dc27371b6335fc6ec61468f1
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2024-06-05 16:23:36 +0200

    try static file middleware

---
+++ src/Simplic.OxS.ModelDefinition.Extension/ModelDefinitionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Simplic.OxS.ModelDefinition.Extension
+{
+    public class ModelDefinitionMiddleware : IMiddleware
+    {
+        private readonly StaticFileMiddleware staticFileMiddleware;
+
+        public ModelDefinitionMiddleware(RequestDelegate next,
+            IWebHostEnvironment env,
+            ILoggerFactory loggerFactory,
+            string basePath)
+        {
+            staticFileMiddleware = ModelDefinitionBuilder.BuildStaticFileMiddleware(next, env, basePath, loggerFactory);
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await staticFileMiddleware.Invoke(context);
+        }
+    }
+}

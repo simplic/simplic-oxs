fb4db585939673ba3e44f37edd713ea83e786124
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2024-08-19 10:42:26 +0200

    add error logging middleware

---
+++ src/Simplic.OxS.Server/Middleware/ErrorLoggingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Simplic.OxS.Server.Middleware;
+
+/// <summary>
+/// Middleware to log every unhandled exception.
+/// </summary>
+public class ErrorLoggingMiddleware(RequestDelegate next, ILogger logger)
+{
+    /// <summary>
+    /// invokes the middleware and logs an exception if any unhandled is thrown.
+    /// </summary>
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            var message = e.Message;
+            logger.LogError(e, "The following error happened: {message}", message);
+            throw;
+        }
+    }
+}

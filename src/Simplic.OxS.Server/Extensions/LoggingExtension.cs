ffb8b039aea192b053a10fae035f092d8cf26655
4eck-qed <ssindustries@web.de>
2024-08-20 17:28:03 +0200

    add exc & no exc variant for every level

--- src/Simplic.OxS.Server/Extensions/LoggingExtension.cs
+++ src/Simplic.OxS.Server/Extensions/LoggingExtension.cs
@@ -30,10 +30,9 @@ public static class LoggingExtension
     /// Writes a log message using the `Trace`/`Verbose` log level.
     /// Used for logging the most minor information.
     /// </summary>
-    /// <param name="logger"></param>
-    /// <param name="message"></param>
-    /// <param name="args"></param>
-    public static void Verbose(this ILogger logger, string? message, params object?[] args)
+    /// <param name="message">Message to log</param>
+    /// <param name="args">Format arguments for message</param>
+    public static void Verb(this ILogger logger, string? message, params object?[] args)
     {
         var eventId = new EventId(-1, Guid.NewGuid().ToString());
         using (CallerScope())
@@ -42,13 +41,22 @@ public static class LoggingExtension
         }
     }
 
+    /// <inheritdoc cref="Verb"/>
+    public static void Verb(this ILogger logger, Exception e, string? message, params object?[] args)
+    {
+        var eventId = new EventId(-1, Guid.NewGuid().ToString());
+        using (CallerScope())
+        {
+            logger.LogTrace(eventId, e, message, args);
+        }
+    }
+
     /// <summary>
     /// Writes a log message using the `Debug` log level.
     /// Used for debugging purposes.
     /// </summary>
-    /// <param name="logger"></param>
-    /// <param name="message"></param>
-    /// <param name="args"></param>
+    /// <param name="message">Message to log</param>
+    /// <param name="args">Format arguments for message</param>
     public static void Debug(this ILogger logger, string? message, params object?[] args)
     {
         var eventId = new EventId(-1, Guid.NewGuid().ToString());
@@ -58,13 +66,22 @@ public static class LoggingExtension
         }
     }
 
+    /// <inheritdoc cref="Debug(ILogger,string?,object?[])"/>
+    public static void Debug(this ILogger logger, Exception e, string? message, params object?[] args)
+    {
+        var eventId = new EventId(-1, Guid.NewGuid().ToString());
+        using (CallerScope())
+        {
+            logger.LogDebug(eventId, e, message, args);
+        }
+    }
+
     /// <summary>
     /// Writes a log message using the `Information` log level.
     /// Used for general logging of noteworthy information.
     /// </summary>
-    /// <param name="logger"></param>
-    /// <param name="message"></param>
-    /// <param name="args"></param>
+    /// <param name="message">Message to log</param>
+    /// <param name="args">Format arguments for message</param>
     public static void Info(this ILogger logger, string? message, params object?[] args)
     {
         var eventId = new EventId(-1, Guid.NewGuid().ToString());
@@ -74,13 +91,22 @@ public static class LoggingExtension
         }
     }
 
+    /// <inheritdoc cref="Info(ILogger,string?,object?[])"/>
+    public static void Info(this ILogger logger, Exception e, string? message, params object?[] args)
+    {
+        var eventId = new EventId(-1, Guid.NewGuid().ToString());
+        using (CallerScope())
+        {
+            logger.LogInformation(eventId, e, message, args);
+        }
+    }
+
     /// <summary>
     /// Writes a log message using the `Warning` log level.
     /// Used for logging unexpected or problematic situations.
     /// </summary>
-    /// <param name="logger"></param>
-    /// <param name="message"></param>
-    /// <param name="args"></param>
+    /// <param name="message">Message to log</param>
+    /// <param name="args">Format arguments for message</param>
     public static void Warn(this ILogger logger, string? message, params object?[] args)
     {
         var eventId = new EventId(-1, Guid.NewGuid().ToString());
@@ -90,14 +116,36 @@ public static class LoggingExtension
         }
     }
 
+    /// <inheritdoc cref="Warn(ILogger,string?,object?[])"/>
+    public static void Warn(this ILogger logger, Exception e, string? message, params object?[] args)
+    {
+        var eventId = new EventId(-1, Guid.NewGuid().ToString());
+        using (CallerScope())
+        {
+            logger.LogWarning(eventId, e, message, args);
+        }
+    }
+
     /// <summary>
     /// Writes a log message using the `Error` log level.
     /// Used for logging exceptions.
     /// </summary>
-    /// <param name="logger"></param>
-    /// <param name="e"></param>
-    /// <param name="message"></param>
-    /// <param name="args"></param>
+    /// <param name="message">Message to log</param>
+    /// <param name="args">Format arguments for message</param>
+    public static void Error(
+        this ILogger logger,
+        string message = "",
+        params object?[] args
+    )
+    {
+        var eventId = new EventId(-1, Guid.NewGuid().ToString());
+        using (CallerScope())
+        {
+            logger.LogError(eventId, message, args);
+        }
+    }
+
+    /// <inheritdoc cref="Error(ILogger,string,object?[])"/>
     public static void Error(
         this ILogger logger,
         Exception e,
@@ -112,15 +160,28 @@ public static class LoggingExtension
         }
     }
 
+
     /// <summary>
     /// Writes a log message using the `Critical`/`Fatal` log level.
     /// Used for logging the most severe errors that should be taken care of immediately.
     /// </summary>
-    /// <param name="logger"></param>
-    /// <param name="e"></param>
-    /// <param name="message"></param>
-    /// <param name="args"></param>
-    public static void Crit(
+    /// <param name="message">Message to log</param>
+    /// <param name="args">Format arguments for message</param>
+    public static void Fatal(
+        this ILogger logger,
+        string message = "",
+        params object?[] args
+    )
+    {
+        var eventId = new EventId(-1, Guid.NewGuid().ToString());
+        using (CallerScope())
+        {
+            logger.LogCritical(eventId, message, args);
+        }
+    }
+
+    /// <inheritdoc cref="Fatal(ILogger,string,object?[])"/>
+    public static void Fatal(
         this ILogger logger,
         Exception e,
         string message = "",
@@ -205,6 +266,8 @@ public static class LoggingExtension
             var callerMethod = callerFrame.GetMethod()!;
             var callerType = callerMethod.ReflectedType!.Name;
             var callerFn = callerMethod.Name;
+            if (callerFn.StartsWith('.'))
+                callerFn = callerFn[1..];
 
             const string fnColor = "\u001b[33m";
             const string classColor = "\u001b[38;2;30;216;184m";

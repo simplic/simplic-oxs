4f3c8e5c25e22ccfa90b60c26014cfca031b0711
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2025-01-29 08:41:09 +0100

    fix request context and better sample

---
+++ src/Simplic.OxS.ApiKeySample/Controllers/ApiKeyTestController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Simplic.OxS.Server.Controller;
+using Simplic.OxS.Server.Extensions;
+
+namespace Simplic.OxS.ApiKeySample.Controllers
+{
+    [ApiController]
+    [ApiKeyOrJwtAuthorize]
+    [Route("[controller]")]
+    public class ApiKeyTestController : OxSController
+    {
+        
+        private readonly IRequestContext requestContext;
+        private readonly ILogger<ApiKeyTestController> _logger;
+
+        public ApiKeyTestController(ILogger<ApiKeyTestController> logger, IRequestContext requestContext)
+        {
+            _logger = logger;
+            this.requestContext = requestContext;
+        }
+
+        [HttpGet(Name = "TestApiKey")]
+        public IActionResult Get()
+        {
+            return Ok(
+                new Response
+                {
+                    OId = requestContext.OrganizationId ?? Guid.Empty,
+                    UId = requestContext.UserId ?? Guid.Empty,
+                });
+         
+        }
+    }
+}

b949c8295dbac6752752b78a7b73bba984103472
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2025-02-07 23:04:40 +0100

    add attribute for jwt only authorization

---
+++ src/Simplic.OxS.Server/Attributes/AuthorizeJwtOnlyAttribute.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Simplic.OxS.Server;
+
+/// <summary>
+/// Attribute for securing controllers or methods from getting called with api keys for authenticaiton.
+/// </summary>
+public class AuthorizeJwtOnlyAttribute() : AuthorizeAttribute(policy: "JwtOnly")
+{
+}

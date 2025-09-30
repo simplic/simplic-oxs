4ea0b393d77bef30a35699c31cfabad179275699
Marvin Eichhoff <marvin.eichhoff@spiegelburg.biz>
2025-09-30 14:56:29 +0200

    added comments

--- src/Simplic.OxS.Settings/Organization/IOrganizationSettingRepository.cs
+++ src/Simplic.OxS.Settings/Organization/IOrganizationSettingRepository.cs
@@ -8,5 +8,9 @@ namespace Simplic.OxS.Settings.Organization;
 /// </summary>
 public interface IOrganizationSettingRepository : IOrganizationRepository<Guid, OrganizationSetting, SettingFilter>
 {
+    /// <summary>
+    /// Retrieves all organization settings.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation, containing a list of organization settings.</returns>
     Task<IEnumerable<OrganizationSetting>> GetAllAsync();
 }

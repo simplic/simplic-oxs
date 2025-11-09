a884c0d9d4df75f30e3005991fdc5c3fa2cc2189
Benedikt Eggers <be@spiegelburg.net>
2025-11-09 23:52:14 +0100

    Add resource actions attribute

---
+++ src/Simplic.OxS/Security/ResourceActionsAttribute.cs
@@ -0,0 +1,34 @@
+﻿namespace Simplic.OxS.Security
+{
+    /// <summary>
+    /// Defines a list of provided actions for a specific resource
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class ResourceActionsAttribute : Attribute
+    {
+        public ResourceActionsAttribute(string name, string[] actions)
+        {
+            Name = name;
+            Actions = actions;
+        }
+
+        /// <summary>
+        /// Gets or sets the resource name. E.g. oxs:logistics.orders
+        /// </summary>
+        public string Name 
+        {
+            get;
+            init;
+        }
+
+        /// <summary>
+        /// Gets or sets a list of available actions.
+        /// For example: read, create, update, delete
+        /// </summary>
+        public string[] Actions 
+        { 
+            get; 
+            init;
+        }
+    }
+}

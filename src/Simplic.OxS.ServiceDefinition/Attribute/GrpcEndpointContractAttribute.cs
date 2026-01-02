39646698f0e71e34089cd00ab0dcb514289acfb1
Benedikt Eggers <be@spiegelburg.net>
2026-01-02 15:35:19 +0100

    Add GrpcEndpointContract attribute

---
+++ src/Simplic.OxS.ServiceDefinition/Attribute/GrpcEndpointContractAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Simplic.OxS.ServiceDefinition;
+
+/// <summary>
+/// Attribute to register a gRPC endpoint contract
+/// </summary>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class GrpcEndpointContractAttribute : EndpointContractAttribute
+{
+    /// <summary>
+    /// Initializes a new instance of the GrpcEndpointContractAttribute class with the specified contract name.
+    /// </summary>
+    /// <param name="contractName">Name of the contract</param>
+    /// <param name="serviceName">Microservice / service name within the k8s network, for example simplic-oxs-document</param>
+    /// <param name="grpcPackage">gRPC package name</param>
+    /// <param name="gprcService">gRPC service name</param>
+    /// <param name="grpcMethod">gRPC method name</param>
+    /// <param name="providerName">Provider name (if multiple provider are supported for the given contract)</param>
+    public GrpcEndpointContractAttribute([NotNull] string contractName, [NotNull] string serviceName, [NotNull] string grpcPackage, [NotNull] string gprcService, [NotNull] string grpcMethod, [NotNull] string providerName)
+        : base(contractName, $"[gprc]http://{serviceName}:8082::{grpcPackage}.{gprcService}::{grpcMethod}", providerName)
+    {
+
+    }
+}
